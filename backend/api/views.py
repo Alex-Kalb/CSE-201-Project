17e6f2e31c03a9974fe7824a5069c95833d0dfc2
Paige <125319150+PaperPaige@users.noreply.github.com>
2023-03-14 22:19:44 -0400

    Updating POST product
    Should now request both the url and category of products that are submitted using the form (attempt 1)

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def get_all_product(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        if not products:    return Response({})
+        serializer = ProductSerializer(products, many=True)
+        
+        return Response(serializer.data)
+    
+@api_view(['POST'])
+def add_account(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+
+        account = Account()
+        account.name = name
+        account.email = email
+        account.phone = phone
+        account.save()
+
+        print(name)
+        return Response({'user_id': account.id},
+                        status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def get_all_account(request):
+    if request.method == 'GET':
+        accounts = Account.objects.all()
+        if not accounts:    return Response({})
+        serializer = AccountSerializer(accounts, many=True)
+        
+        return Response(serializer.data)
+    
+@api_view(['POST'])
+def add_order(request):
+    if request.method == 'POST':
+        print("Start")
+        user_id = request.POST.get('user_id')
+        product_name = request.POST.get('product_name')
+        condition = request.POST.get('condition')
+        price = request.POST.get('price')
+        url = request.POST.get('url')
+        category = request.POST.get('category')
+        print("Got data from request")
+        order = Order(order_type='s', order_status='a', order_owner=Account.objects.get(id=user_id))
+        order.save()
+        print("Save order")
+
+
+        product = Product.objects.create(name=product_name, condition=condition, 
+            url=url, category=category, price=price, order_sell=order)
+
+        return Response({'order_id': order.id},
+                        status=status.HTTP_201_CREATED)
