65a862adb816c0b8afbc9dd383cda9f19685fd9f
Paige <125319150+PaperPaige@users.noreply.github.com>
2023-04-04 13:24:07 -0400

    Adds sort by name and price (products)

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+rom django.shortcuts import render
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
@@ -52,16 +52,33 @@ def add_order(request):
         product_name = request.POST.get('product_name')
         condition = request.POST.get('condition')
         price = request.POST.get('price')
-        url = request.POST.get('url')
-        category = request.POST.get('category')
         print("Got data from request")
         order = Order(order_type='s', order_status='a', order_owner=Account.objects.get(id=user_id))
         order.save()
         print("Save order")
 
 
-        product = Product.objects.create(name=product_name, condition=condition, 
-            url=url, category=category, price=price, order_sell=order)
+        product = Product.objects.create(name=product_name, condition=condition, price=price, order_sell=order)
 
         return Response({'order_id': order.id},
                         status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def sort_products_by_name(request):
+    if request.method == 'GET':
+        products = Product.objects.all().order_by('name')
+        if not products:
+            return Response({})
+        serializer = ProductSerializer(products, many=True)
+        
+        return Response(serializer.data)
+
+@api_view(['GET'])
+def sort_products_by_price(request):
+    if request.method == 'GET':
+        products = Product.objects.all().order_by('price')
+        if not products:
+            return Response({})
+        serializer = ProductSerializer(products, many=True)
+        
+        return Response(serializer.data)
